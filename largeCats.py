5daac49b52370a6f64048ccd43dcedec1e3ecd1d
NathanL21 <90645133+NathanL21@users.noreply.github.com>
2022-01-13 18:24:59 +0000

    Initial commit

---
+++ largeCats.py
@@ -0,0 +1,7 @@
+class largeCats:
+  def __init__(self, _color, _species, _toothsize, _size):
+    self.color = _color
+    self.species = _species
+    self.toothsize = _toothsize
+    self.size = _size
+    
---
+++ main.py
@@ -0,0 +1,7 @@
+from largeCats import largeCats
+
+
+#def __init__(self, _color, _species, _toothsize, _size):
+
+mylargeCats=largeCats("yellow","cheetah","medium","large")
+
